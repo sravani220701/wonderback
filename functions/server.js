ab2a248f75c787362c5f55f78683a52af5d6dfe0
sravani220701 <smumm5@unh.newhaven.edu>
2024-12-09 00:46:12 -0500

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,49 +1,61 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-const serverless = require('serverless-http');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const {MongoClient} = require('mongodb');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI;
+const MONGO_URI ="mongodb+srv://sravanimummaneni:mummanenisravani@cluster0.m0ksz.mongodb.net/wonders?retryWrites=true&w=majority&appName=Cluster0";
+const mongo_client_connection = new MongoClient(MONGO_URI);
 
-mongoose.connect(mongoURI).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
+
+const server = http.createServer((req, res) =>{
+    console.log(req.url);
+    if(req.url ==='/'){ // home page
+
+        fs.readFile(path.join(__dirname,'../public','index.html'),(err,content)=>{
+
+        if (err) throw err ;
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(content)
+         })
+    }  
+    else if (req.url === '/api') {
+        const headers =
+        {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+            "Content-Type": 'application/json'
+        };
+        (async(req,res)=>{
+            try
+            {
+                // connect to the MongoDB cluster
+                await mongo_client_connection.connect();
+                const wonders_collection = mongo_client_connection.db("wonders").collection(wonders);
+                
+                const cursor = wonders_collection.find({});
+                const results = await cursor.toArray();
+                const js = (JSON.stringify(results));
+                res.writeHead(200, headers);
+                res.end(js);
+                console.log(js);
+            }catch (err) {
+                console.error(err);
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('server failed to handle the request');
+              } finally {
+                await mongo_client_connection.close();
+              }
+        })(req,res);
+     }
+    else{
+        res.end("<h1> 404 Nothing is here </h1>")
+    }
+
 });
 
-const wonderSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  location: String,
-  establishedYear: Number,
-  additionalDetail: String,
-  imageUrl: String
-});
+const PORT= process.env.PORT || 1617;
 
-const Wonder = mongoose.model('Wonder', wonderSchema);
+// port, callback
+server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
 
-app.use(express.static(path.join(__dirname, 'public')));
-  
-app.get('/api', async (req, res) => {
-  console.log('Received request on /api');
-  try {
-    const wonders = await Wonder.find();
-    res.json(wonders);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).send(error);
-  }
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-module.exports.handler = serverless(app);
