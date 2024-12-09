76859aeeb5239c62ab31c2a1e497c92c44583094
sravani220701 <smumm5@unh.newhaven.edu>
2024-12-09 01:12:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) =>{
     console.log(req.url);
     if(req.url ==='/'){ // home page
 
-        fs.readFile(path.join(__dirname,'../public','index.html'),(err,content)=>{
+        fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
 
         if (err) throw err ;
         res.writeHead(200, {'Content-Type': 'text/html'})
